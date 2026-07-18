ade1c62a45e802a3f7e7d3859dccf5f4f6daa892
Leandro Lescano <leandro.lescano1988@gmail.com>
2026-07-18 01:13:28 -0300

    Se agrego la seccion Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProjectCard from "./components/ProjectCard";
 import projects from "./data/projects";
 import "./App.css";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
                 </div>
             </section>
             <Contact />
+            <Footer />
 
         </>
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>© 2026 Leandro Carrion Lescano</p>
+            <p>Desarrollado con React + Vite</p>
+        </footer>
+    );
+}
+
+export default Footer;
