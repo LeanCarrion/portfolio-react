887104e2e969fcf2fa14b25b896b12c0fa7138c4
Leandro Lescano <leandro.lescano1988@gmail.com>
2026-07-28 16:52:38 -0300

    se reescribio about y se puse estilo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,20 @@
+import "./About.css";
+
 function About() {
     return(
-        <><h1>
-        SOBRE MI
-        </h1>
-        <p>Soy estudiante de Ingeniería en Sistemas de la Información en UTN Rosario.
-        Me interesa el desarrollo de software y la c de datos</p></>
+        <section className="About">
+            <><h1>
+                SOBRE MI
+            </h1>
+        <p>Soy estudiante de Ingeniería en Sistemas en la Universidad Tecnológica Nacional (UTN), 
+            con interés en análisis de datos, desarrollo de software y resolución de problemas mediante tecnología.
+            Actualmente estoy ampliando mis conocimientos en Python, SQL, Power BI y React mientras 
+            desarrollo proyectos personales que me permiten aplicar lo aprendido durante la carrera.
+            Mi objetivo es orientar mi carrera hacia Data Analytics, combinando una sólida formación en 
+            ingeniería con herramientas de análisis y visualización de datos para transformar información
+             en soluciones útiles.</p></>
+        </section>
+        
     )
 }
 
